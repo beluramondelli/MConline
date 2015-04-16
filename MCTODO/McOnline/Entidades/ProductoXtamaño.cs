329057ecee351338dc7d6d6b47d4e0ead5adc30c
agusdemalde <agus.demalde@gmail.com>
2015-04-16 17:15:59 -0300

    productoXtamaño

--- MCTODO/McOnline/Entidades/ProductoXtamaño.cs
+++ MCTODO/McOnline/Entidades/ProductoXtamaño.cs
@@ -5,7 +5,22 @@ using System.Text;
 
 namespace Entidades
 {
-    class ProductoXtamaño
+   public class ProductoXtamaño
     {
+        public int id_producto { get; set; }
+        public int id_tamaño { get; set; }
+        public float precio { get; set; }
+
+        public ProductoXtamaño()
+        {
+    }
+        public ProductoXtamaño(int id_producto, int id_tamaño, float precio)
+        {
+            this.id_producto = id_producto;
+            this.id_tamaño = id_tamaño;
+            this.precio = precio;
+        }
+
+
     }
 }
