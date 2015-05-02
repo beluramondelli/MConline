0f7467d3431a68896796ff11d3e40f91e4af8cce
beluramondelli <belu.ramondelli@gmail.com>
2015-05-02 20:47:30 -0300

    esta es la que subi a somee

--- MCTODO/McOnlines/pedido2.aspx.cs
+++ MCTODO/McOnlines/pedido2.aspx.cs
@@ -11,164 +11,171 @@ using System.Data.SqlClient;
 using System.Windows.Forms;
 
 
-
-public partial class pedido2 : System.Web.UI.Page
-{
-    public static DataTable dt;
-    public static List<ProductoXpedido> listProdXped;
-
-    protected void Page_Load(object sender, EventArgs e)
+    public partial class pedido2 : System.Web.UI.Page
     {
-        if (!IsPostBack)
+        public static DataTable dt;
+        public static List<ProductoXpedido> listProdXped;
+
+        protected void Page_Load(object sender, EventArgs e)
         {
-            dt=new DataTable();
-            dt.Columns.Add("Nombre");
-            dt.Columns.Add("Cantidad");
-            dt.Columns.Add("Descripción");
-            dt.Columns.Add("Precio");
-            listProdXped = new List<ProductoXpedido>();
-
-            CargarGrilla();
-            txtCantidad.Enabled = false;
-            txtDescripcion.Enabled = false;
-            btnAgregarCarrito.Enabled = false;
-            btnConfirmar.Enabled = false;
-            txtTotal.Enabled = false;
-            lblCant.Visible = false;
-         }
-    }
-
-    private void CargarGrilla()
-    {
-        try
-        {
-            gvProductos.DataSource = ProductoDAO.ObtenerTodo();
-            gvProductos.DataKeyNames = new string[] { "id_producto" };
-            gvProductos.DataBind();
-
-            //gvProductos.Columns[2].Visible = false;
-            gvProductos.HeaderRow.Cells[2].Text = " Producto";
-            gvProductos.HeaderRow.Cells[3].Text = " Descripcion";
-            gvProductos.HeaderRow.Cells[4].Text = " Precio";
-           
-        }
-        catch
-        {
-
-        }
-    }
-    protected void gvProductos_SelectedIndexChanged(object sender, EventArgs e)
-    {
-        txtCantidad.Text = "1";
-        txtDescripcion.Text = "";
-        txtCantidad.Enabled = true;
-        txtDescripcion.Enabled = true;
-        btnAgregarCarrito.Enabled = true;
-    }
-
-    protected void btnAgregarCarrito_Click(object sender, EventArgs e)
-    {
-        if (txtCantidad.Text != "")
-        {
-
-            if (int.Parse(txtCantidad.Text) > 0 && int.Parse(txtCantidad.Text) < 101)
+            if (!IsPostBack)
             {
-                bool ban = validarDescripcion();
-                if (ban == true)
+                dt = new DataTable();
+                dt.Columns.Add("Nombre");
+                dt.Columns.Add("Cantidad");
+                dt.Columns.Add("Descripción");
+                dt.Columns.Add("Precio");
+                listProdXped = new List<ProductoXpedido>();
+
+                CargarGrilla();
+                txtCantidad.Enabled = false;
+                txtDescripcion.Enabled = false;
+                btnAgregarCarrito.Enabled = false;
+                btnConfirmar.Enabled = false;
+                txtTotal.Enabled = false;
+                lblCant.Visible = false;
+            }
+        }
+
+        private void CargarGrilla()
+        {
+            try
+            {
+                gvProductos.DataSource = ProductoDAO.ObtenerTodo();
+                gvProductos.DataKeyNames = new string[] { "id_producto" };
+                gvProductos.DataBind();
+
+                //gvProductos.Columns[2].Visible = false;
+                gvProductos.HeaderRow.Cells[2].Text = " Producto";
+                gvProductos.HeaderRow.Cells[3].Text = " Descripcion";
+                gvProductos.HeaderRow.Cells[4].Text = " Precio";
+
+            }
+            catch
+            {
+
+            }
+        }
+        protected void gvProductos_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            txtCantidad.Text = "1";
+            txtDescripcion.Text = "";
+            txtCantidad.Enabled = true;
+            txtDescripcion.Enabled = true;
+            btnAgregarCarrito.Enabled = true;
+        }
+
+        protected void btnAgregarCarrito_Click(object sender, EventArgs e)
+        {
+            if (txtCantidad.Text != "")
+            {
+
+                if (int.Parse(txtCantidad.Text) > 0 && int.Parse(txtCantidad.Text) < 101)
                 {
-                    ProductoXpedido pxp = new ProductoXpedido();
-                    pxp.descripcion = txtDescripcion.Text;
-                    pxp.cantidad = int.Parse(txtCantidad.Text);
+                    bool ban = validarDescripcion();
+                    if (ban == true)
+                    {
+                        ProductoXpedido pxp = new ProductoXpedido();
+                        pxp.descripcion = txtDescripcion.Text;
+                        pxp.cantidad = int.Parse(txtCantidad.Text);
 
-                    lblCant.Visible = false;
-                    int id_producto = (int.Parse(gvProductos.SelectedDataKey.Value.ToString()));
-                    string nom = ProductoDAO.ObtenerProductoPorId(id_producto);
-                    int precio = ProductoDAO.obtenerPrecioProducto(id_producto);
-                    int cantidad = (int.Parse(txtCantidad.Text.ToString()));
-                    int subtotal = (precio * cantidad);
+                        lblCant.Visible = false;
+                        int id_producto = (int.Parse(gvProductos.SelectedDataKey.Value.ToString()));
+                        string nom = ProductoDAO.ObtenerProductoPorId(id_producto);
+                        int precio = ProductoDAO.obtenerPrecioProducto(id_producto);
+                        int cantidad = (int.Parse(txtCantidad.Text.ToString()));
+                        int subtotal = (precio * cantidad);
 
-                    pxp.precio = subtotal;
-                    pxp.id_producto = id_producto;
-                    pxp.id_tamaño = 3;
+                        pxp.precio = subtotal;
+                        pxp.id_producto = id_producto;
+                        pxp.id_tamaño = 3;
 
-                    listProdXped.Add(pxp);
+                        listProdXped.Add(pxp);
 
-                    dt.Rows.Add(nom, pxp.cantidad, pxp.descripcion, subtotal);
-                    cargarGrilla(dgvCarrito, dt);
-                    CalcularTotal();
-                    btnConfirmar.Enabled = true;
-                    txtCantidad.Text = "";
-                    txtCantidad.Enabled = false;
-                    txtDescripcion.Text = "";
-                    txtDescripcion.Enabled = false;
-                    btnAgregarCarrito.Enabled = false;
+                        dt.Rows.Add(nom, pxp.cantidad, pxp.descripcion, subtotal);
+                        cargarGrilla(dgvCarrito, dt);
+                        CalcularTotal();
+                        btnConfirmar.Enabled = true;
+                        txtCantidad.Text = "";
+                        txtCantidad.Enabled = false;
+                        txtDescripcion.Text = "";
+                        txtDescripcion.Enabled = false;
+                        btnAgregarCarrito.Enabled = false;
+                    }
+                    else
+                    {
+                        lblCant.Text = "La descripcion supera el maximo de caracteres permitidos";
+                        lblCant.Visible = true;
+                    }
+
                 }
                 else
                 {
-                    lblCant.Text = "La descripcion supera el maximo de caracteres permitidos";
+                    lblCant.Text = "La cantidad ingresada debe ser un valor entre 1 y 100";
                     lblCant.Visible = true;
                 }
-
             }
             else
             {
-                lblCant.Text = "La cantidad ingresada debe ser un valor entre 1 y 100";
+                lblCant.Text = "Debe ingresar una cantidad";
                 lblCant.Visible = true;
             }
         }
-        else
+
+        private void cargarGrilla(GridView gv, DataTable dt)
         {
-            lblCant.Text = "Debe ingresar una cantidad";
-            lblCant.Visible = true;
+            gv.DataSource = dt;
+            gv.DataBind();
         }
-    }
 
-    private void cargarGrilla(GridView gv, DataTable dt)
-    {
-        gv.DataSource = dt;
-        gv.DataBind();
-    }
 
-  
-    protected void gvProductos_PageIndexChanged(object sender, EventArgs e)
-    {}
+        protected void gvProductos_PageIndexChanged(object sender, EventArgs e)
+        { }
 
-    protected void gvProductos_PageIndexChanging(object sender, GridViewPageEventArgs e)
-    {
-        gvProductos.PageIndex = e.NewPageIndex;
-        CargarGrilla();
-    }
-    protected void btnConfirmar_Click(object sender, EventArgs e)
-    {
-        Pedido ped = new Pedido();
-
-        ped.fecha=DateTime.Now;
-        ped.montoTotal = int.Parse(txtTotal.Text);
-        ped.id_pedido = 0;
-        ped.horaPedido = DateTime.Now;
-        ped.horaEntrega = DateTime.Now;
-
-        McDAO.PedidoDAO.insertarPedido(ped, listProdXped);
-        Response.Redirect("~/PedidoRealizado.aspx");
-
-    }
-
-    public void CalcularTotal()
-    {
-        int total = 0;
-        for (int i = 0; i < dgvCarrito.Rows.Count; i++)
+        protected void gvProductos_PageIndexChanging(object sender, GridViewPageEventArgs e)
         {
-            total = total + int.Parse(dgvCarrito.Rows[i].Cells[4].Text);
+            gvProductos.PageIndex = e.NewPageIndex;
+            CargarGrilla();
         }
-        txtTotal.Text = total.ToString();
-    }
+        protected void btnConfirmar_Click(object sender, EventArgs e)
+        {
+            gvConfirmar.Visible = true;
+            btnAceptar.Visible = true;
+            cargarGrilla(gvConfirmar, dt);
 
-    public bool validarDescripcion()
-    {
-        txtDescripcion.MaxLength = 100;
-        if (txtDescripcion.Text.Length < txtDescripcion.MaxLength)
-            return true;
-        else
-            return false;
-    }
+        }
+
+        public void CalcularTotal()
+        {
+            int total = 0;
+            for (int i = 0; i < dgvCarrito.Rows.Count; i++)
+            {
+                total = total + int.Parse(dgvCarrito.Rows[i].Cells[4].Text);
+            }
+            txtTotal.Text = total.ToString();
+        }
+
+        public bool validarDescripcion()
+        {
+            txtDescripcion.MaxLength = 100;
+            if (txtDescripcion.Text.Length < txtDescripcion.MaxLength)
+                return true;
+            else
+                return false;
+        }
+
+        protected void btnAceptar_Click(object sender, EventArgs e)
+        {
+            Pedido ped = new Pedido();
+
+            ped.fecha = DateTime.Now;
+            ped.montoTotal = int.Parse(txtTotal.Text);
+            ped.id_pedido = 0;
+            ped.horaPedido = DateTime.Now;
+            ped.horaEntrega = DateTime.Now;
+
+            McDAO.PedidoDAO.insertarPedido(ped, listProdXped);
+            Response.Redirect("~/PedidoRealizado.aspx");
+        }
+    
 }
