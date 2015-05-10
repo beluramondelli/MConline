7e3f770b15512fd46a1d63d53c4082b5cfafc436
beluramondelli <belu.ramondelli@gmail.com>
2015-05-10 13:39:28 -0300

    cerrar sesion

--- MCTODO/McOnlines/Site.master.cs
+++ MCTODO/McOnlines/Site.master.cs
@@ -9,8 +9,31 @@ public partial class SiteMaster : System.Web.UI.MasterPage
 {
     protected void Page_Load(object sender, EventArgs e)
     {
-
+        if (!IsPostBack)
+        {
+            if (Session["usuario"] != null)
+            {
+                lblbienvenido.Text = "Bienvenido " + Session["usuario"].ToString();
+                HyperLink1.Visible = false;
+                HyperLink2.Visible = false;
+                HyperLink3.Visible = true;
+            }
+            else
+            {
+                lblbienvenido.Visible = false;
+                HyperLink1.Visible = true;
+                HyperLink2.Visible = true;
+                HyperLink3.Visible = false;
+            }
+        }
     }
+
+    protected void cerrarSesion(object sender, EventArgs e)
+    {
+        Session["usuario"] = null;
+        Response.Redirect("~/Principal.aspx");
+    }
+
     protected void NavigationMenu_MenuItemClick(object sender, MenuEventArgs e)
     {
 
